3f4819122eda499fa343fe554417bb5a8920e474
HaykKarapetyan00 <134404361+HaykKarapetyan00@users.noreply.github.com>
2023-07-06 20:13:47 +0400

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	char a;
+	scanf("%c", &a);
+	if(a == 65 || a == 97 || a == 69 || a == 101 || a == 73 || a == 105 || a == 79 || a == 111 || a== 85 || a == 117 || a== 89 || a== 121){
+		printf("dzaynavor e");
+	}
+	else{
+		printf("baxadzayn e");
+	}
+}
---
+++ hw3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+	if(a == b || a == c || b == c){
+		if(a >= b && a >= c){
+			printf("%d",a);
+		}
+		else if(b >= c && b >= a){
+			printf("%d",b);
+		}
+		else if(c >= b && c >= a){
+			printf("%d" ,c);
+		}
+	}
+	else{
+		printf("chkan mutqagrvac tver voronq bavavrum en xndri paymannerin");
+	}
+}
